45baab76cccc8553037e470e80182697ef0b3173
videogames4all <AWCBaker@gmail.com>
2015-02-11 12:31:14 -0700

    README added and fixing up stuff on the shell

--- totw_p2/shell.c
+++ totw_p2/shell.c
@@ -32,14 +32,12 @@ int main(int argc, char *argv[]) {
   char *in = NULL;
   char *next_cmd = NULL;
   int i = 0;
-  int status = 0;
+  //int status = 0;
   char *prompt = "totw> ";
   FILE *file = stdin;
   char **commands = NULL;
   int cmd_count = 0;
   int *pids = NULL;
-
-  int error = 0; /* The value execvp returns if invalid command */
 
   if(argc == 2) {
     file = fopen(argv[1], "r");
@@ -102,81 +100,27 @@ int main(int argc, char *argv[]) {
 
 /* Runs a command and returns the PID of the new process. */
 int run_cmd(char *cmd) {
-  /* Parse args */
-    args[0] = strtok(in, " ");
-    
-    for(i = 1; (args[i] = strtok(NULL, " ")) != NULL; i++)
-      ;
+  int error = 0; /* The value execvp returns if invalid command */
 
-    if((pid = fork()) != 0) {
-      if(wait(&status) != pid)
-	;
-    } else {
-      /* Check if there is a semicolon in the arguments, before executing the arguments */
-      /* If so, we want to execute the arguments after the semicolon separately */
-
-      //How many arguments are there?
-      while(args[arguments] != NULL) {
-	arguments++;
-      }
-
-      /* Now we can find where a semicolon is */
-      for(i = 0; i < arguments; i++){
-	if(strcmp(args[i], ";") == 0){
-	  /* Hey we found a semicolon */
-	  semis++;
-	  
-	  /* Everything from next_arg to i - 1 is a (hopefully) valid command */
-	  /* Store it all into short list */
-	  int k = 0;
-	  for(j = next_arg; j < i; j++) {
-	    short_list[k] = args[j];
-	    k++;
-	  }
-	  short_list[k] = NULL;
-	  /* Let's use a fork for concurrency */
-	  pid_t cmd = fork();
-	  if(cmd >= 0) {
-	    if(cmd == 0){
-	      error = execvp(short_list[0], short_list);
-	      error = is_error(error);
-	      exit(0);
-	    }
-	  }
-	  else {
-	    perror("Forking failed.\n");
-	  }
-	  
-	  /* Set the next argument counter */
-	  next_arg = i + 1;
-	  /* printf("i:%d\n", i); */
-	}
-      }
-      if(semis == 0) {
-	error = execvp(args[0], args);
-	error = is_error(error);
-
-	should_run = 0;
-      } else {
-	/* Account for the last argument */
-	int k = 0;
-	for(j = next_arg; j < arguments; j++) {
-	  short_list[k] = args[j];
-	  k++;
-	}
-	short_list[k] = NULL;
-	error = execvp(short_list[0], short_list);
-	error = is_error(error);
-
-	should_run = 0;
-      }
+  /* Let's use a fork for concurrency */
+  pid_t forker = fork();
+  if(forker >= 0) {
+    if(forker == 0){
+      error = execvp(cmd[0], cmd);
+      error = is_error(error);
     }
-    /**
-     *After reading user input, the steps are:
-     *(1) fork a child process using fork()
-     *(2) the child process will invoke execvp()
-     *(3) if command include &, parent will invoke wait()
-     */
+  }  else {
+    perror("Forking failed.\n");
+  }
+
+  /**
+  *After reading user input, the steps are:
+  *(1) fork a child process using fork()
+  *(2) the child process will invoke execvp()
+  *(3) if command include &, parent will invoke wait()
+  */
+
+  return 0;
 }
 
 //Returns a pointer to a new string consisting of a null terminator.

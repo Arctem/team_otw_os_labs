d6d489cf75586089f54c20171484618f77dfecb7
Russell White <rarctem@gmail.com>
2015-02-18 12:48:14 -0600

    Initial commit for Project 3.

---
+++ totw_p3/p3.c
@@ -0,0 +1,27 @@
+/*****************************************************
+ * Filename: p3.c
+ *
+ * Description: 
+ *
+ * Team: Omega Tiger Woods
+ * Authors: Andrew Baker and Russell White
+ * Class: CSE 325
+ * Instructor: Zheng
+ * Assignment: Lab Project #3
+ * Assigned: January 11, 2015
+ * Due: February 25, 2015
+ *****************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+  /* 1. Get command line arguments */
+  /* 2. Initialize buffer entries with -1 */
+  /* 3. Create producer thread(s) */
+  /* 4. Create consumer thread(s) */
+  /* 5. Sleep 300 seconds */
+  /* 6. Exit */
+  return 0;
+}

0349c8e2724f92838eb3b11589763f4039fbc9b4
Russell White <rarctem@gmail.com>
2015-02-18 17:22:39 -0600

    Implement buffer functions add_to, pop, is_empty, and is_full. Also move buffer functions to buffer.c and buffer.h.

---
+++ totw_p3/buffer.c
@@ -0,0 +1,49 @@
+/*****************************************************
+ * Filename: buffer.c
+ *
+ * Description: 
+ *
+ * Team: Omega Tiger Woods
+ * Authors: Andrew Baker and Russell White
+ * Class: CSE 325
+ * Instructor: Zheng
+ * Assignment: Lab Project #3
+ * Assigned: January 11, 2015
+ * Due: February 25, 2015
+ *****************************************************/
+
+#include "buffer.h"
+
+/* Stack will add to the start of the list, */
+/* queue will add to the end. */
+void add_to(buffer *buff, int value) {
+  int i;
+  
+  if(buff->stack) {
+    for(i = BUFFER_SIZE - 1; i > 0; i++)
+      buff->data[i] = buff->data[i - 1];
+    buff->data[0] = value;
+  } else {
+    for(i = 0; buff->data[i] != -1; i++)
+      ;
+    buff->data[i] = value;
+  }
+}
+
+int pop(buffer *buff) {
+  int i;
+  int retval = buff->data[0];
+  for(i = 0; i < BUFFER_SIZE - 1; i++)
+    buff->data[i] = buff->data[i + 1];
+  
+  return retval;
+}
+
+/* -1 indicates no value, so if we see one it's empty */
+int is_empty(buffer *buff) {
+  return buff->data[0] == -1;
+}
+
+int is_full(buffer *buff) {
+  return buff->data[BUFFER_SIZE - 1] == -1;
+}

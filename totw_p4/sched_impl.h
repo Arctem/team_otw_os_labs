e4a9d23364007c0ad367a87f6801471283df38f1
videogames4all <AWCBaker@gmail.com>
2015-03-02 12:19:40 -0700

    Added the provided support for doubly-linked lists and cleaned up the tabbed comments that Zheng did in sched_impl.h

--- totw_p4/sched_impl.h
+++ totw_p4/sched_impl.h
@@ -1,12 +1,15 @@
 #ifndef	__SCHED_IMPL__H__
 #define	__SCHED_IMPL__H__
 
+#include "list.h"
+
 struct thread_info {
-	/*...Fill this in...*/
+  /*...Fill this in...*/
 };
 
 struct sched_queue {
-	/*...Fill this in...*/
+  /*...Fill this in...*/
+  list_t *queue;
 };
 
 #endif /* __SCHED_IMPL__H__ */

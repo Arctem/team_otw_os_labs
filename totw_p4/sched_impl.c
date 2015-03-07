d80120b24d32306acd507bcaa7fd8feb4ee0c989
Russell White <rarctem@gmail.com>
2015-03-07 17:56:14 -0600

    Remove some debug. Make run without leaks and errors for -fifo 1 1 1. Add find_in_list to find something in the list. Change compiler to clang for reasons.

--- totw_p4/sched_impl.c
+++ totw_p4/sched_impl.c
@@ -17,7 +17,10 @@ void insert_item_tail(list_t *lst, void *datum) {
 
 void *peek_item_head(list_t *lst) {
   list_elem_t *elt = list_get_head(lst);
-  return elt->datum;
+  if(elt)
+    return elt->datum;
+  else
+    return NULL;
 }
 
 void *peek_item_tail(list_t *lst) {
@@ -36,19 +39,30 @@ int elt_in_list(list_t *lst, void *datum) {
   return 0;
 }
 
+list_elem_t *find_in_list(list_t *lst, void *datum) {
+  list_elem_t *elt = list_get_head(lst);
+  while(elt != NULL) {
+    if(elt->datum == datum)
+      return elt;
+    else
+      elt = elt->next;
+  }
+  return NULL;
+}
+
 void remove_item(list_t *lst, void *datum) {
-  list_elem_t *elt = malloc(sizeof(list_elem_t));
-  list_elem_init(elt, datum);
-  list_remove_elem(lst, elt);
-  free(elt);
+  list_elem_t *elt = find_in_list(lst, datum);
+  if(elt) {
+    list_remove_elem(lst, elt);
+    free(elt);
+  }
 }
 
 /* Start of thread functions */
 
 static void init_thread_info(thread_info_t *info, sched_queue_t *queue) {
-  printf("Initialized thread struct.\n");
   info->sq = queue;
-  info->sem = malloc(sizeof(sem_t));
+  info->sem = calloc(1, sizeof(sem_t));
   sem_init(info->sem, 0, -1); /* initialize semaphore as busy */
 }
 
@@ -59,7 +73,6 @@ static void destroy_thread_info(thread_info_t *info) {
 
 static void enter_sched_queue(thread_info_t *info) {
   insert_item_tail(info->sq->queue, info);
-  printf("Semaphore %p\n", info->sq->queue_sem);
   sem_post(info->sq->queue_sem);
 }
 
@@ -79,16 +92,15 @@ static void release_cpu(thread_info_t *info) {
 /* Start of scheduler functions */
 
 static void init_sched_queue(sched_queue_t *queue, int queue_size) {
-  printf("Initialized schedule struct.\n");
   queue->queue = malloc(sizeof(list_t));
   list_init(queue->queue);
   queue->max_running = queue_size;
   queue->running = malloc(sizeof(list_t));
   list_init(queue->running);
   
-  queue->sem = malloc(sizeof(sem_t));
+  queue->sem = calloc(1, sizeof(sem_t));
   sem_init(queue->sem, 0, queue_size - 1); /* initialize semaphore based on size */
-  queue->queue_sem = malloc(sizeof(sem_t));
+  queue->queue_sem = calloc(1, sizeof(sem_t));
   sem_init(queue->queue_sem, 0, -1); /* initialize semaphore to wait on threads */
 }
 
@@ -110,7 +122,7 @@ static void wait_for_worker(sched_queue_t *queue) {
 }
 
 static thread_info_t *next_worker(sched_queue_t *queue) {
-  list_elem_t *elt = peek_item_head(queue->queue);
+  list_elem_t *elt = list_get_head(queue->queue);
   while(elt != NULL) {
     if(!elt_in_list(queue->running, elt->datum))
       return elt->datum;
@@ -123,7 +135,6 @@ static thread_info_t *next_worker(sched_queue_t *queue) {
 static void wait_for_queue(sched_queue_t *queue) {
   if(list_size(queue->queue) == 0) {
     /* reinitialize semaphore to wait on threads */
-    printf("Semaphore %p\n", queue->queue_sem);
     sem_init(queue->queue_sem, 0, -1);
     sem_wait(queue->queue_sem);
   }

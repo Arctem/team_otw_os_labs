891e0654b34fc4a0c46544ed013af49af17ba87f
Russell White <rarctem@gmail.com>
2015-05-03 18:52:44 -0500

    Add description of how our file system will be stored.

--- totw_p6/disk.h
+++ totw_p6/disk.h
@@ -31,6 +31,33 @@ int block_read(int block, char *buf);
                                /* read a block of size BLOCK_SIZE from disk   */
 /******************************************************************************/
 
+/* File system structure:
+ * 4096 blocks of "meta" (most unused).
+ * 4096 blocks of data (use depends on files that have been created).
+ *
+ * Block 0 of meta data is a simple byte list representing which data blocks
+ * are currently in use. Each byte is either 0 (false) or 1 (true) and corresponds
+ * to one of the data blocks. That is, byte i represents the status of data block
+ * i (located at block 4096 + i).
+ *
+ * Blocks 1 through 192 are used to store file data. These blocks are divided into
+ * 64 3-block groups, as there is a limit of 64 files in the file system at any
+ * given time. The first block of each group is an info block while the second two
+ * store the list of blocks used by that file.
+ * The structure of the info block is as follows:
+ * 1 byte containing either 0 (false) or 1 (true) representing if the file exists.
+ * 25 bytes representing the file's name. Is null-terminated if under 25 characters long.
+ * A short of 2 bytes indicating how many blocks the file uses.
+ * A short of 2 bytes indicating how much of the final block the file uses.
+ *
+ * The structure of the list blocks are as follows:
+ * 2048 2-byte shorts representing a data block the file uses. A file's data blocks
+ * may not necessarily be sequential. Both blocks together contain 4096 entries, allowing
+ * for a file to potentially use up all 4096 data blocks. Theoretically all entries past
+ * the number of blocks indicated by the grou's info block should be 0, but they will
+ * be ignored so their exact value should not be important.
+ */
+
 int make_fs(char* disk_name); /* make a file system */
 int mount_fs(char* disk_name); /* mount a file system */
 int umount_fs(char* disk_name); /* un-mount a file system */

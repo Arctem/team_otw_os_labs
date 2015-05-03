61f6fd034e6c0fcbc5cb62fab8fb38fab8642194
Russell White <rarctem@gmail.com>
2015-05-03 18:35:26 -0500

    Test now opens and closes a file system.

--- totw_p6/test.c
+++ totw_p6/test.c
@@ -3,6 +3,8 @@
 #include "disk.h"
 
 int main(int argc, char* argv[]) {
-  assert(make_disk("disk.dat") == 0);
+  assert(make_fs("disk.dat") == 0);
+  assert(mount_fs("disk.dat") == 0);
+  assert(umount_fs("disk.dat") == 0);
   return 0;
 }

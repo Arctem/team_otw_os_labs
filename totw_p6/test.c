372a4f1a5b53ea4eec71fc5460701150d782c025
Russell White <rarctem@gmail.com>
2015-05-03 22:51:40 -0500

    Add a bit of testing with opening/closing files.

--- totw_p6/test.c
+++ totw_p6/test.c
@@ -4,11 +4,20 @@
 #include "fs.h"
 
 int main(int argc, char* argv[]) {
+  int f = 0;
+  
   assert(make_fs("disk.dat") == 0);
   assert(mount_fs("disk.dat") == 0);
   assert(fs_create("file.test") == 0);
+  f = fs_open("file.test");
+  assert(f != -1);
+  assert(fs_close(f) == 0);
   assert(umount_fs("disk.dat") == 0);
+
   assert(mount_fs("disk.dat") == 0);
+  f = fs_open("file.test");
+  assert(f != -1);
+  assert(fs_close(f) == 0);
   assert(umount_fs("disk.dat") == 0);
   return 0;
 }
